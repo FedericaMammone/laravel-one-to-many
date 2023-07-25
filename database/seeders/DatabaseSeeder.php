f9821ec4c8cfa507b50a1a2290dc213bef13947e
Fef97 <federicakr97@gmail.com>
2023-07-25 10:27:52 +0200

    invertiti dati per l'id

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,8 +22,9 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this->call([
-            ProjectTableSeeder::class,
-            TypeTableSeeder::class
+            TypeTableSeeder::class,
+            ProjectTableSeeder::class
+
         ]);
     }
 }
